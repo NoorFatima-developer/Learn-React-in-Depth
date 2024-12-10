8d8dc720c1fd6f249caf0b7464269d6e3bde2be5
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-11 02:20:43 +0500

    code is added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
   
+  // Ab main kahani hai inject js in html...ku k at the end react h e jsx...
+  // inject js variables in react...
   return (
    <>
    <h1>Chai aur React | Noor Fatima </h1>
