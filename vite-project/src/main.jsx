81d60514bf884b02ade40fd159ca2c60f2c84976
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-11 01:21:47 +0500

    code is added

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,8 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+function MyApp(){
+  return (
+    <div>
+      <h1>My First React App</h1>
+      <p>Welcome to the Chai aur React website</p>
+    </div>
+  )
+}
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
