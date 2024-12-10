f8f31bc6f586ea08927cbf941657ac7f8c2340a2
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-11 01:46:16 +0500

    code is added

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -14,15 +14,32 @@ function MyApp(){
   )
 }
 
+// m sochogi yahan pr k jsx babel compiler k through jsx ko js m convert kr rha hai...
+// tu aghr m direct e osko js ka code dedo tu kea pta oska 1 step bach jye or fer b code run o jye tu m ye try krogi
+// TRY:
 
+const createElement = {
+  type: 'a',
+  props: {
+      href: "https://www.google.com",
+      target: "_blank"
+  },
+  Children: "Visit Google"
+}
 
+const anotherElement = (
+  <a href="http://google.com" target="_blank">Visit Google</a>
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MyApp />
+    {/* <MyApp /> */}
+    {/* lkin ye tu kam e ni kr rha ku k isk andr object hau chlo new func bana k dekhti o */}
+    {/* <createElement /> */}
   </StrictMode>,
 )
-  // React akhir h tu js e h na so we can do this below as well...
+
+// React akhir h tu js e h na so we can do this below as well...
 //   createRoot(document.getElementById('root')).render(
 //     MyApp()
 //   )
