e4d9f3e6ec3b409a73779d3db2ac0c93ebaa91d4
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-11 01:28:39 +0500

    code is added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,6 @@ function App() {
   return (
    <>
    <h1>Chai aur React | Noor Fatima </h1>
-   <p>Welcome to the Chai aur React website</p>
    </>
   )
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// Ab compiler jo h babel wo jsx yani js k andr jo ye html code h osko js m convert kr rha hai behind the scene...
+
 
 function MyApp(){
   return (
@@ -17,7 +19,12 @@ function MyApp(){
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>,
 )
+  // React akhir h tu js e h na so we can do this below as well...
+//   createRoot(document.getElementById('root')).render(
+//     MyApp()
+//   )
+// )
 
