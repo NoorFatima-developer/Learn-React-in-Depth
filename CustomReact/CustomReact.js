875878cdb61bca5649155f934ae8bdbddc41c8be
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-11 00:54:01 +0500

    code is added

--- CustomReact/CustomReact.js
+++ CustomReact/CustomReact.js
@@ -1,3 +1,12 @@
+function customReact(reactElement, container){
+
+    // Create an element in DOM
+    const domelement = document.createElement(reactElement.type);
+    domelement.innerHTML = reactElement.Children;
+}
+
+
+
 
 // Basically browser jsx ko understand ni krta wo bs js ko understand krta hai
 // islye hum jsx ko js m convert krty hain by using createElement ta k hmara browser jsx ko js smj sky...
@@ -15,4 +24,4 @@ const mainContainer = document.querySelector("#root");
 
 // Now inject react in html:
 
-customReact(createElement, createElement);
+customReact(createElement, mainContainer);
