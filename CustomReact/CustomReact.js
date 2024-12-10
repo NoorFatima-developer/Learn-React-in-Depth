315a96b54972c6c4ae3e02769b2a3454a6155ac9
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-10 22:58:34 +0500

    code is added

--- CustomReact/CustomReact.js
+++ CustomReact/CustomReact.js
@@ -1,1 +1,11 @@
+// Create Element in React...
+const createElement = {
+    type: 'a',
+    props: {
+        href: "https://www.google.com",
+        target: "_blank"
+    },
+    Children: "Visit Google"
+}
+
 const mainContainer = document.querySelector("#root");
