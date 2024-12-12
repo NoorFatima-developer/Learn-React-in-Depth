813dc3505d903b391dd9d4868dbe95e219a1c270
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-12 06:02:41 +0500

    code is added

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   // ye essy kam ni kr rye ga, ye state sy kam kryga...
   // let counter = 15;
 
-  const [counter, setcounter] = useState(15)
+  let [counter, setCounter] = useState(15)
 
   const addValue = () => {
     // Counter++ sy console m tu updation hori hai, lkin UI update nahi horha... So, UI ko update krny klye we use react hooks:
@@ -19,15 +19,37 @@ function App() {
 
     //print 15 ku k meny counter ki value bdmai update ki h
     //  console.log("Clicked: " , counter);
-     setcounter(counter + 1);
-     console.log("Clicked: " , counter);
-  }
+    setCounter(prevCounter => {
+     const newCounter = prevCounter + 1;
+     console.log("Clicked: " , newCounter);
+     return newCounter > 20 ? 20 : newCounter;
+  });
+}
   
-  function removeValue() {
-    // counter--;
-    setcounter(counter - 1);
-    console.log("Clicked: " , counter);
+  // TWO WAYS:
+
+  // Aghr tu m return krwana chahti o tu phr m as a callback logi setCounter ko..
+  // and aghr mai return ni krwana chahti o tu phr m 2nd method use kr skti o..
+  // 1--------------
+  // function removeValue() {
+  //   setCounter(prevCounter =>{
+  //   const newCounter = prevCounter - 1;
+  //   console.log("Clicked: " , newCounter);
+  //   return newCounter < 0 ? 0 : newCounter;
+  // })
+  // }
+
+  // 2--------------
+  function removeValue(){
+  const newCounter = counter - 1;
+  console.log("Clicked: " , newCounter);
+  setCounter(newCounter < 0? 0 : newCounter);
+  // essy b condition add krskti o mai:
+  // if(newCounter < 0){
+  //   setCounter(0);
+  // }
   }
+
     return (
       <>
         <h1>Chai aur Code</h1>
