a7a197c1777017d6db8ca9215c6721ff76573ed9
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-15 04:43:37 +0500

    code is added

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,43 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   // ye essy kam ni kr rye ga, ye state sy kam kryga...
   // let counter = 15;
 
-  let [counter, setCounter] = useState(15)
+  let [counter, setCounter] = useState(15);
 
   const addValue = () => {
     // Counter++ sy console m tu updation hori hai, lkin UI update nahi horha... So, UI ko update krny klye we use react hooks:
     // So, we can say k UI updation ko React control krta hai...
-  
+
     // console.log("Value is added", Math.random());
     // counter++;
 
     //print 15 ku k meny counter ki value bdmai update ki h
     //  console.log("Clicked: " , counter);
 
-  // One more thing ye h k aghr m setCounter k andr prevCounter na likhti or essy counter++ krti tu ye update na kerta...
-  // wo in sb counters as a batch leta..wo dekhta k sbk andr same e kam hora hai tu wo isko single hi smjta...
-  // setCounter(counter + 1)
-  // setCounter(counter + 1)
-  // setCounter(counter + 1)
-  // setCounter(counter + 1)
-  // Islye k aghr mery pss koi esi condition aa e gi h jismai mjhy different  kam krwany hain tu osklye i will use setCounter as a callback like below:
+    // One more thing ye h k aghr m setCounter k andr prevCounter na likhti or essy counter++ krti tu ye update na kerta...
+    // wo in sb counters as a batch leta..wo dekhta k sbk andr same e kam hora hai tu wo isko single hi smjta...
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // Islye k aghr mery pss koi esi condition aa e gi h jismai mjhy different  kam krwany hain tu osklye i will use setCounter as a callback like below:
 
-    setCounter(prevCounter => {
-     const newCounter = prevCounter + 1;
-     const newCounter1 = newCounter+1;
-     const newCounter2 = newCounter1 + 1;
-     const finalCounter = newCounter2> 20 ? 20 : newCounter2
-     console.log("Clicked: " , finalCounter);
-     return finalCounter;
-  });
-}
-  
+    setCounter((prevCounter) => {
+      const newCounter = prevCounter + 1;
+      const newCounter1 = newCounter + 1;
+      const newCounter2 = newCounter1 + 1;
+      const finalCounter = newCounter2 > 20 ? 20 : newCounter2;
+      console.log("Clicked: ", finalCounter);
+      return finalCounter;
+    });
+  };
+
   // TWO WAYS:(Same for above means add value)
 
   // Aghr tu m return krwana chahti o tu phr m as a callback logi setCounter ko..
@@ -55,29 +54,28 @@ function App() {
   // }
 
   // 2--------------
-  function removeValue(){
-  const newCounter = counter - 1;
-  const finalCounter = setCounter(newCounter < 0? 0 : newCounter);
-  console.log("Clicked: " , finalCounter);
-  return finalCounter;
+  function removeValue() {
+    const newCounter = counter - 1;
+    const finalCounter = setCounter(newCounter < 0 ? 0 : newCounter);
+    console.log("Clicked: ", finalCounter);
+    return finalCounter;
 
-  // essy b condition add krskti o mai:
-  // if(newCounter < 0){
-  //   setCounter(0);
-  // }
+    // essy b condition add krskti o mai:
+    // if(newCounter < 0){
+    //   setCounter(0);
+    // }
   }
 
-    return (
-      <>
-        <h1>Chai aur Code</h1>
-        <h2>Counter Value: {counter}</h2>
+  return (
+    <>
+      <h1>Chai aur Code</h1>
+      <h2>Counter Value: {counter}</h2>
 
-        <button onClick={addValue}>Add Value {counter}</button>
-        <br />
-        <button onClick={removeValue}>Remove Value {counter}</button>
-      </>
-    )
-    
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/Card'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-
   const myObj = {
-    name: 'Chai',
+    name: "Chai",
     age: 28,
-    hobbies: ['Coding', 'Reading', 'Travelling']
-  }
+    hobbies: ["Coding", "Reading", "Travelling"],
+  };
 
-  let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <>
-   <h1 className='bg-green-500 text-black p-4 rounded-xl mb-4'>tailwindcss</h1>
-   {/* Aghr mai direct object ya array pass krti tu wo error deta islye mai oskooper bana k nichy {} k thrugh access kr ri o... */}
-   {/* <Card channel = "Chai aur Code" someArr = {newArr} someObje = {myObj} /> */}
-   <Card username = "chaiaurcode" btnText = "Click Me"/>
-   <Card username = "Noorfatima" btnText = "Visit Me"/>
-   {/* Ye oss case m hai button jb mai props button pass nahi kr rhi... */}
-   {/* <Card username = "Noorfatima"/> */}
-   
+      <h1 className="bg-green-500 text-black p-4 rounded-xl mb-4">
+        tailwindcss
+      </h1>
+      {/* Aghr mai direct object ya array pass krti tu wo error deta islye mai oskooper bana k nichy {} k thrugh access kr ri o... */}
+      {/* <Card channel = "Chai aur Code" someArr = {newArr} someObje = {myObj} /> */}
+      <Card username="chaiaurcode" btnText="Click Me" />
+      <Card username="Noorfatima" btnText="Visit Me" />
+      {/* Ye oss case m hai button jb mai props button pass nahi kr rhi... */}
+      {/* <Card username = "Noorfatima"/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
