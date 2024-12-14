80a2362ed79da4122dc77f270fe0a40a754b7b03
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-15 03:25:27 +0500

    code is added

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -6,10 +6,27 @@ function App() {
   return (
     <div className="w-full h-screen duration-200"
     style={{backgroundColor: color}}>
-      <div className='fixed flex flex-wrap justify-center inset-x-0 px-2 bottom-12'>test</div>
-      
+      <div className='fixed flex flex-wrap justify-center inset-x-0 px-2 bottom-12'>
+     <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+      <button
+        className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+        style={{backgroundColor: "red"}}
+      >Red</button>
+      <button
+        className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+        style={{backgroundColor: "green"}}
+      >Green</button>
+      <button
+        className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+        style={{backgroundColor: "blue"}}
+      >Blue</button>
+      <button
+        className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+        style={{backgroundColor: "yellow"}}
+      >Yellow</button>
+      </div> 
     </div>
-    
+    </div>
   )
 }
 
