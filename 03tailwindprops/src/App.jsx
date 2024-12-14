bddb7f53fdeab7e2c7791f4049bc2ea36aec6f33
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-14 21:24:18 +0500

    code is added

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,10 +6,18 @@ import Card from './components/Card'
 
 function App() {
 
+  const myObj = {
+    name: 'Chai',
+    age: 28,
+    hobbies: ['Coding', 'Reading', 'Travelling']
+  }
+
+  let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   return (
     <>
    <h1 className='bg-green-500 text-black p-4 rounded-xl mb-4'>tailwindcss</h1>
-   <Card/>
+   <Card channel = "Chai aur Code" someArr = {newArr} someObje = {myObj} />
    <Card/>
     </>
   )
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 function cards(props) {
   console.log("props", props);
-  
   return (
     <div>
       <div class="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
