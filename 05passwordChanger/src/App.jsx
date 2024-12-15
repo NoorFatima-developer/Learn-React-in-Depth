8b4e72e61e3fec1d4ab3e3411a0641ce9280214d
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-15 17:32:36 +0500

    code is added

---
+++ 05passwordChanger/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-black'>
+        <h1 className='text-6xl'>Password Changer</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
