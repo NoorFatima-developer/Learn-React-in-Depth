908497f7782237a2b003744326de39cb73bc50fc
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-16 20:43:22 +0500

    code is added

--- 05passwordChanger/src/App.jsx
+++ 05passwordChanger/src/App.jsx
@@ -1,12 +1,13 @@
-import './index.css'
-
+import '../dist/output.css';  
+// import './index.css'
+// import '../src/index.css'
 function App() {
 
   return (
     <>
-      <div className='bg-black'>
-        <h1 className='text-6xl'>Password Changer</h1>
-        </div>
+      <div className='bg-black w-full h-screen flex justify-center'>
+        <h1 className='text-6xl text-white text-center'>Password Changer</h1>
+      </div>
     </>
   )
 }
--- 05passwordChanger/tailwind.config.js
+++ 05passwordChanger/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"], // Add 'jsx' here
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
