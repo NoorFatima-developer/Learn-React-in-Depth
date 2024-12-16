25b3f29cb23380f1135bba454d8274d206ec03c6
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-16 23:29:01 +0500

    code is added

--- 05passwordChanger/src/App.jsx
+++ 05passwordChanger/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -9,11 +9,28 @@ function App() {
   const [charAllowed, setCharAllowed] = useState('false');
   const [password, setPassword] = useState('');
 
+  function passwordGenerator = useCallback(( ) => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123';
+
+    if (numberAllowed === 'true') {
+      str += '0123456789';
+    }
+
+    if (charAllowed === 'true') {
+      str += '!@#$%^&*()_+=-{}[]|:;"<>,.?/~`';
+    }
+
+   for (let i = 1; i <= array.length; i++) {
+     pass += str.charAt(Math.floor(Math.random() * str.length + 1));
+    
+   }
+
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
   return (
     <>
-
       <h1 className='text-4xl text-center'>Test</h1>
-    
     </> 
   )
 }
