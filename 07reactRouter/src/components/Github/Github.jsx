1c4c2f7b03c1374152b24a69faf3c677ba1dcc73
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 03:08:53 +0500

    code is added

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Github() {
 
     function GitHub(){
-        fetch('https://api.github.com/users/NoorFatima-developer')
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
+        // fetch data from github api
+        // and update state with the fetched data
 
+        // Fetch data and render by using useEffect...
+        useEffect( () => {
+            useEffect( () => {
+                fetch('https://api.github.com/NoorFatima-developer')
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    setData(data);
+                })
+            }, [])
+        })
+    }
 
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
