83b983898899864804cf40e2be3ea7bbc935f90f
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 19:14:14 +0500

    code is added

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -1,7 +1,10 @@
-import {React,UserContext}  from 'react'
+import {React,useContext}  from 'react'
 import UserContext from '../context/UserContext'
 
 function Profile() {
+    // Ab ye components haina tu components k andr hum log 2 chezo ko import krty hain
+    // 1---useContext ko...
+    //2----UserCpntext ko (jahan osko create kea hota hai oska path...)
   const {user} = useContext(UserContext)
 
   return (
