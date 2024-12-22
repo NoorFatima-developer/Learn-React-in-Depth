75df7b72c5d194e9200e45a8b872bd7b098bb5da
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 19:04:24 +0500

    code is added

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -4,8 +4,17 @@ function Login() {
     const [username, setusername] = useState([])
     const [password, setpassword] = useState([])
 
+    // Ab hum useContext ko apny components k andr kesy use krygy..jesy ye mai login.jsx k andr use kr rhi hun tu m kesy krogi...
+    // Tu osklye i will do this:
+
+    // ye setUser whi hai jo hmny UserContextProvider k andr pass kea hai...
+    const {setUser} = useContext(UserContext)
+
     // handleSubmit function to handle form submission
     const handleSubmit = () => {
+        // By default jb b hum log submit krty hain tu value "url" k through kahi na kahi chly jti hai POST method k through..
+        // Tu I want k value kahi b na jye tu osklue I will use this default behaviour...
+        e.preventDefault();
 
     }
   return (
