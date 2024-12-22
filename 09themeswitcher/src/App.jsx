91f2202906d77a72f88608f72723f667e1b092f4
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-23 01:46:06 +0500

    code is added

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -3,13 +3,22 @@ import './App.css'
 import { ThemeProvider } from './contexts/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () =>{
+    setThemeMode("light")
+  }
+
+  const darkTheme = () =>{
+    setThemeMode("dark")
+  }
 
   return (
 
     // ............Consumer........
     // Pehly context creat k way m hum values ko provider k andr e pass krty thy yahan pr hum during consumer pass krygy...
-    <ThemeProvider value={{themeMode, lightMode, darkMode}}>
+    // Ab pichly context API k andr hmny jo b provider k andr methods bananye thy wo define b thy ab ye tu define ni tu osklye hum 1 1 ko pkr k seoarate func bana k osmai daal degy tu yr b same oski trhan hi behave krygy....
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
          <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
