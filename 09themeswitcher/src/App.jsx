15848a33dcd3df84ccf94cb530959c019fd35ffa
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-23 01:40:15 +0500

    code is added

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
 
     // ............Consumer........
-
+    // Pehly context creat k way m hum values ko provider k andr e pass krty thy yahan pr hum during consumer pass krygy...
     <ThemeProvider value={{themeMode, lightMode, darkMode}}>
          <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
