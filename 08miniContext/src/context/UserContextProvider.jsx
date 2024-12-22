dbd8e9a2317c0dc48f62957d89586818ff42d7db
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 18:14:27 +0500

    code is added

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider = ()=> {
 
+// Way 02... Provider....
+const UserContextProvider = (children)=> {
+
+    const [user, setUser] = useState(null)
+    return(
+        // isklye hum value mai object pass krty hain...and object m wo data hota hai jisko hmny pass krna hota hai....
+        <UserContext.Provider value={{user, setUser}}>
+            {props.children}
+        </UserContext.Provider>
+    )
 }
 
 export default UserContextProvider;
