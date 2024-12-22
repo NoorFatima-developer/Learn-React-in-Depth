41eba2e3e423c2896e7a15d3250503feeb3b716c
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 19:22:09 +0500

    code is added

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -3,13 +3,14 @@ import UserContext from "./UserContext";
 
 
 // Way 02... Provider....
-const UserContextProvider = (children)=> {
+// children 1 variable  hai m kuch or b leksti and children mnz div and all data jo k fragment k andr wrap hota hai that;s it...
+const UserContextProvider = ({children})=> {
 
     const [user, setUser] = useState(null)
     return(
         // isklye hum value mai object pass krty hain...and object m wo data hota hai jisko hmny pass krna hota hai....
         <UserContext.Provider value={{user, setUser}}>
-            {props.children}
+            {children}
         </UserContext.Provider>
     )
 }
