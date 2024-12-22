36c267b7bd6951c80fa725c6a9ec31c9e4698684
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 18:05:02 +0500

    code is added

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+// CreateContext basically hamara global context hai jisk andr hum apny saray data ko wrap krdety hain
+// ye 1 provider ki trhan behave krta hai....wrapper b keh skyt hain isk andr hmny sary app.jsx k data ko wrap krna hota hai...
+// tu osklye hmy 1 or filke banannni prygi jsx k nam sy ...
+
+const UserContext = React.createContext()
+
+export default UserContext;
