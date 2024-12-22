f85997a3aa75f0673a8952c741d57570d2e114ea
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 18:24:05 +0500

    code is added

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+
+    // 03 Way... Consumer...
     <UserContextProvider>
       <h1>Context API</h1>
     </UserContextProvider>
