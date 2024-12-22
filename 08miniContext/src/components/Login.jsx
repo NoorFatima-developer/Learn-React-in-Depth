106d85d94c9afc2330f3c3de4c669d170c442891
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 18:59:37 +0500

    code is added

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-
+import React, { useState, useContext } from 'react'
+import UserContext  from '../context/UserContext';
 function Login() {
     const [username, setusername] = useState([])
     const [password, setpassword] = useState([])
