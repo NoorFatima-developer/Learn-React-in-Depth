902e58409e0afa30741ac1d1e8b4ad0d958af187
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-22 19:10:28 +0500

    code is added

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -8,6 +8,8 @@ function Login() {
     // Tu osklye i will do this:
 
     // ye setUser whi hai jo hmny UserContextProvider k andr pass kea hai...
+    // yahan hmny wo method pass krna hai jo hmy lena hai jo k {setuser hai}
+    // tu abi hmny provide kea hai ab access b krygy jis file m krna o lkin wo access hum log krygy {user} sy ku k hmy data lena hai na k ab ki dfa method...
     const {setUser} = useContext(UserContext)
 
     // handleSubmit function to handle form submission
@@ -15,14 +17,14 @@ function Login() {
         // By default jb b hum log submit krty hain tu value "url" k through kahi na kahi chly jti hai POST method k through..
         // Tu I want k value kahi b na jye tu osklue I will use this default behaviour...
         e.preventDefault();
-
+        setUser({username, password})
     }
   return (
     <div>
       <h2>Login</h2>
       {/* Abi tk values hawa mai hain wo hum govern krygy values ko by using value and onchange... */}
-      <input type="text" value={username} onChange={(e) => username(e.target.value)} placeholder='username'/>
-      <input type="password" value={password} onChange={(e) => password(e.target.value)} placeholder='password'/>
+      <input type="text" value={username} onChange={(e) => setusername(e.target.value)} placeholder='username'/>
+      <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} placeholder='password'/>
       <button onClick={handleSubmit}>Submit</button>
     </div>
   )
