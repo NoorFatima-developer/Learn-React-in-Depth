b36cf76dc62e6eba049d02ea37a30646b730e036
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-27 21:42:38 +0500

    code is added

--- 11ReduxToolkit/src/app/components/Todos.jsx
+++ 11ReduxToolkit/src/app/components/Todos.jsx
@@ -4,9 +4,22 @@ import { removeTodo } from '../features/todo/todoSlice';
 
 
 function Todos() {
-    useSelector(state => state.todos)
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+
   return (
+    <>
     <div>Todos</div>
+    {/* ab todos k andr list of items show hongy islye loop laga deti o */}
+    {todos.map((todo) => (
+        <li key={todo.id}>
+            {todo.text}
+            {/* jb b hmy koi method ya parameters pass krny hon tu hum callback firekrskty hain ku k hmy function ka reference dena hai na k call krna hai
+            essy dispatch() */}
+            <button onClick={}></button>
+        </li>
+    ))}
+    </>
   )
 }
 
