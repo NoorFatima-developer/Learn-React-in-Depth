4e97e69d78962c112e7ce8ca706cc17fa064dea6
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-27 20:57:51 +0500

    code is added

--- 11ReduxToolkit/src/app/features/todo/todoSlice.js
+++ 11ReduxToolkit/src/app/features/todo/todoSlice.js
@@ -1,12 +1,9 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
-
+import { act } from "react";
 
 const initialState = {
   todos: [{id:1, text: "Hello world"}]
 };
-
-
-
 
 export const todoSlice = createSlice({
     name: 'todo',
@@ -15,7 +12,11 @@ export const todoSlice = createSlice({
     // ab jb hum context api k andr hum function bananty thy tu hum tb funciton ki definiiton ni likhty thy lkin isk andr mai definiiton b likhogi...
     reducers: {
         addTodo: (state, action) => {
-
+            const todo = {
+                id: nanoid(),
+                // payload apny apmai 1 object hai tu hum iss kk andr sy kuch b access krskty hain dot laga k...
+                text: action.payload.text
+            }
         },
 
         removeTodo: (state, action) => {
