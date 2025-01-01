f3a2726225ab7f96949aa462134da2520827061e
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-02 03:56:58 +0500

    code is acded

---
+++ 12ReactHooks/src/Hooks.jsx
@@ -0,0 +1,28 @@
+// UseState:
+
+// Ye hamari state ko UI pr update krta hai..
+
+// Use Effect
+
+// jesy e hamara coponent render hota hai tu UseEffect run hota hai baki hum ismai empty array dal skti hai osko rokny klye...
+// Baki API sy data wagaira fetch krwana ho tu osklye b useEffect use hota hai...
+
+// UseRef
+
+// Ye hmry DOM ko manipulate krta hai...
+// Isko Code sy smjty haun...Ismai hum reference dety hain onchange mai mostly ye inputs mai use hota hai...
+
+import React, { useRef, useState } from 'react';
+
+const refElement = useRef("");
+const [name, setName] = useState("Noor");
+
+
+return
+<>
+    <h1>Learning UseRef</h1>
+    <input type='text' value={name}></input>
+
+
+</>
+
