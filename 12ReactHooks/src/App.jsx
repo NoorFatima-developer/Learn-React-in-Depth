9b32b4102e3cf57628549b347108cffefe6f2c6d
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-02 17:33:00 +0500

    code is added

--- 12ReactHooks/src/App.jsx
+++ 12ReactHooks/src/App.jsx
@@ -13,24 +13,37 @@ import './App.css'
 
 //03... useRef => Dom manipulate... reference dety hain hum ismai..
 // jesy meny clipboard sy koi chez copy krni o kisi chez ko highlight krna o k itni chez zopy krni hai, ya phr focus dena hai tb hum zda tr useRef ka refrence dety hain and hum isko use krty hain...
-function App() {
-  const refElement = useRef("");
-  const [name, setName] = useState("Noor")
+// function App() {
+//   const refElement = useRef("");
+//   const [name, setName] = useState("Noor")
 
-  function Reset(){
-    setName("");
-    refElement.current.focus();
-  }
+//   function Reset(){
+//     setName("");
+//     refElement.current.focus();
+//   }
 
-  return (
-    <>
-     <h1>Learning UseRef</h1>
-     <input type='text' value={name} onChange={(e) => setName(e.target.value)} ref={refElement}></input>
-     <button onClick={Reset}>Reset</button>
-     <button onClick={handleInput}>Handle Input</button>
-    </>
-  )
-}
+//   function handleInput(){
+//     refElement.current.focus();
+//     refElement.current.style.color = "red"
+//     refElement.current.value = "Noorah";
+//   } 
 
-export default App
+//   return (
+//     <>
+//      <h1>Learning UseRef</h1>
+//      <input type='text' value={name} onChange={(e) => setName(e.target.value)} ref={refElement}></input>
+//      <button onClick={Reset}>Reset</button>
+//      <button onClick={handleInput}>Handle Input</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+// 04--- UseMemo Hook => Increase Applucation Performance ...Reuse Function, it means jb hum kisi func ko 1 dfa use krty hain tu it stores that function in cache memory and jb hmy dubraa need hoti h tu hum
+// log ossi function ko reuse krlety hain instead of k hum new function ko create krain...
+
+
+
 
