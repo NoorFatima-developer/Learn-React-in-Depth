69429f1661d42c03a171f4159954ec301c4d9ea4
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-02 06:05:40 +0500

    code is added

--- 12ReactHooks/src/App.jsx
+++ 12ReactHooks/src/App.jsx
@@ -3,13 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+// 01...UseState => Update UI...
+
+
+// 02... Jb b hamara component render hota hai tb hi hamara useEffect run hota hai...
+// API sy data fetch krna ho tb b hum useEffect ka use krty hain...
+
+
+//03... useRef => Dom manipulate... reference dety hain hum ismai..
 function App() {
-  const [name, setName] = useState(0)
+  // const refElement = useRef("");
+  const [name, setName] = useState("Noor")
+
+  function Reset(){
+    setName("")
+  }
 
   return (
     <>
      <h1>Learning UseRef</h1>
-     <input type='text' value={name}></input>
+     <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
+     <button onClick={Reset}>Reset</button>
     </>
   )
 }
